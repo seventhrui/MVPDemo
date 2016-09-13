c397fc1b60976d323047845fd4207cf68b30d8ee
seventh <gao_rui2011@163.com>
2016-09-13 16:02:12 +0800

    下拉筛选菜单

---
+++ dropdownmenu/src/main/java/com/tribe7/dropdownmenu/ConstellationAdapter.java
@@ -0,0 +1,73 @@
+package com.tribe7.dropdownmenu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ConstellationAdapter extends BaseAdapter {
+    private Context context;
+    private List<String> list;
+    private int checkItemPosition = 0;
+
+    public void setCheckItem(int position) {
+        checkItemPosition = position;
+        notifyDataSetChanged();
+    }
+
+    public ConstellationAdapter(Context context, List<String> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_constellation_layout, null);
+            viewHolder.mText = (TextView) convertView.findViewById(R.id.text);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        fillValue(position, viewHolder);
+        return convertView;
+    }
+
+    private void fillValue(int position, ViewHolder viewHolder) {
+        viewHolder.mText.setText(list.get(position));
+        if (checkItemPosition != -1) {
+            if (checkItemPosition == position) {
+                viewHolder.mText.setTextColor(context.getResources().getColor(R.color.drop_down_selected));
+                viewHolder.mText.setBackgroundResource(R.drawable.check_bg);
+            } else {
+                viewHolder.mText.setTextColor(context.getResources().getColor(R.color.drop_down_unselected));
+                viewHolder.mText.setBackgroundResource(R.drawable.uncheck_bg);
+            }
+        }
+    }
+
+    static class ViewHolder {
+        TextView mText;
+    }
+}
